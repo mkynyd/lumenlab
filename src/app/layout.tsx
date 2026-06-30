c1120e646d21a69333920978d51b4a54d169f65a
Junhang Yin <yinjunhang@outlook.com>
2026-06-30 10:11:07 +0800

    chore: enable smooth scroll behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,7 @@ export default async function RootLayout({
   return (
     <html
       lang="zh-CN"
+      data-scroll-behavior="smooth"
       className={cn("h-full", "antialiased", "font-sans", figtree.variable)}
       suppressHydrationWarning
     >
